b89e0d39e085e94ee78951268eb3e53e4e521f81
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-09-15 22:47:39 +0200

    up

---
+++ src/main/java/com/mycompany/HelperClasses/Student.java
@@ -0,0 +1,40 @@
+package com.mycompany.HelperClasses;
+
+public class Student {
+    
+    private String name;
+    
+    //initializing the enum
+    private YearLevel year;
+
+    public Student(String name, YearLevel year)
+    {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+
+    public YearLevel getYear() 
+    {
+        return year;
+    }
+    
+    public void setYear(String newYear)
+    {
+        name = newYear;
+    }
+    
+    public void displayInfo() 
+    {
+        System.out.println("Student: " + name + " enrolled in: " + year);
+    }
+}
